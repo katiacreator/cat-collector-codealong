85861107e6f5d6b6c40193275e519c5eb5ffe8e6
katiacreator <katia.creator@gmail.com>
2021-08-27 11:57:43 -0400

    just built templates base.html and about.html with base.html

---
+++ main_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  
+# Define the about view
+def about(request):
+   return render(request, 'about.html')
